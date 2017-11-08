ad48f1276738d1821af3b7e72e078ef19b2d00fe
Adrian <ahingson1@gmail.com>
2017-11-08 15:28:36 -0800

    Woring on Hompage button

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,9 @@ class Homepage extends Component {
       <div className="App">
         <header className="App-header">
           
-         <img src= "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ohio_State_Buckeyes_logo.svg/100px-Ohio_State_Buckeyes_logo.svg.png" align="Left" />
+         <img src= "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ohio_State_Buckeyes_logo.svg/70px-Ohio_State_Buckeyes_logo.svg.png" align="Left" />
          <img id="Img" src="img.jpg" onClick="myFunction()" />
-         <script> document.getElementById( "Img" ).onclick = function() {
-    // img clicked
-};
-</script>
+        
           <h1 className="App-title"></h1>
         </header>
         <p className="App-intro">
