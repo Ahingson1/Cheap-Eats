fabc1eb290acff3a3fbb46b9e8c45602a3a69691
Adrian <ahingson1@gmail.com>
2017-11-14 15:23:06 -0800

    Also added signin page
    Update part 2

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import index from "./index"
 
+
 class Homepage extends Component {
   render() {
     return (
@@ -11,12 +12,14 @@ class Homepage extends Component {
         <header className="App-header">
            
           <a>
-         <label> <img src= "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ohio_State_Buckeyes_logo.svg/60px-Ohio_State_Buckeyes_logo.svg.png" align="Middel" onClick="Img" />
+         <label onClick="img">  <img src= 
+         "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ohio_State_Buckeyes_logo.svg/60px-Ohio_State_Buckeyes_logo.svg.png" align="Middel" onClick="Img" />
          </label>
          
          </a>   
               
-      <button type="button"></button>
+            
+      
         
           <h1 className="App-title">Ohio State Buckeyes Social Network</h1>
         </header>
